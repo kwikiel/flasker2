7d4915cd0e2d43dc587795ab2c9e67bfcea22475
kwikiel <random8902@gmail.com>
2015-02-08 11:02:07 -0500

    Last before forms

--- hello.py
+++ hello.py
@@ -1,9 +1,27 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+from flask.ext.bootstrap import Bootstrap
 
-@app.route('/')
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'cykuvhibjhvkucjx'
+bootstrap = Bootstrap(app)
+
+
+class NameForm(Form):
+    name = StringField('What is your name? ', validators=[Required()])
+    submit = StringField('Submit')
+
+@app.route('/index')
 def index():
-    return 'Lubie placki'
+    return render_template('index.html')
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
